0852fc8d9c33ded12dc6b5b0fc20fe4c2764a1d5
pseudoO97 <aboulbarge.omar@gmail.com>
2022-05-31 23:15:32 +0200

    feat:(backend and frontend)

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,36 +1,76 @@
 import React from 'react'
-import { Navbar, Nav, Container } from 'react-bootstrap'
+import { Route, Routes } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
 import { LinkContainer } from 'react-router-bootstrap'
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
+import SearchBox from './SearchBox'
+import { logout } from '../actions/userActions'
 
 const Header = () => {
-  return (
-    <>
-      <header>
-        <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
-          <Container>
-            <LinkContainer to='#'>
-              <Navbar.Brand>MarocFiscal</Navbar.Brand>
-            </LinkContainer>
+  const dispatch = useDispatch()
 
-            <Navbar.Toggle aria-controls='navbarScroll' />
-            <Navbar.Collapse className='justify-content-end'>
-              <Nav className='ml-auto my-2 my-lg-0'>
-                <LinkContainer to='/cart'>
-                  <Nav.Link>
-                    <i className='fas fa-shopping-cart'></i> Cart
-                  </Nav.Link>
-                </LinkContainer>
+  const userLogin = useSelector((state) => state.userLogin)
+  const { userInfo } = userLogin
+
+  const logoutHandler = () => {
+    dispatch(logout())
+  }
+
+  return (
+    <header>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+          <LinkContainer to='/'>
+            <Navbar.Brand>ProShop</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Routes>
+              <Route
+                render={({ history }) => <SearchBox history={history} />}
+              />
+            </Routes>
+
+            <Nav className='ml-auto'>
+              <LinkContainer to='/cart'>
+                <Nav.Link>
+                  <i className='fas fa-shopping-cart'></i> Cart
+                </Nav.Link>
+              </LinkContainer>
+              {userInfo ? (
+                <NavDropdown title={userInfo.name} id='username'>
+                  <LinkContainer to='/profile'>
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                  </LinkContainer>
+                  <NavDropdown.Item onClick={logoutHandler}>
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : (
                 <LinkContainer to='/login'>
-                  <Nav.Link href='/login'>
+                  <Nav.Link>
                     <i className='fas fa-user'></i> Sign In
                   </Nav.Link>
                 </LinkContainer>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-      </header>
-    </>
+              )}
+              {userInfo && userInfo.isAdmin && (
+                <NavDropdown title='Admin' id='adminmenu'>
+                  <LinkContainer to='/admin/userlist'>
+                    <NavDropdown.Item>Users</NavDropdown.Item>
+                  </LinkContainer>
+                  <LinkContainer to='/admin/productlist'>
+                    <NavDropdown.Item>Products</NavDropdown.Item>
+                  </LinkContainer>
+                  <LinkContainer to='/admin/orderlist'>
+                    <NavDropdown.Item>Orders</NavDropdown.Item>
+                  </LinkContainer>
+                </NavDropdown>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
   )
 }
 
